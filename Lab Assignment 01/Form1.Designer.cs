77422b8baebbb242fc9dd2cb652165ff8cf60deb
jacob-2244 <muhammadyaqoob2580@gmail.com>
2024-11-06 18:12:51 +0500

    files added

---
+++ Lab Assignment 01/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace Scientific_Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxFunctions = new System.Windows.Forms.ComboBox();
+            this.textInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBoxFunctions
+            // 
+            this.comboBoxFunctions.AutoCompleteCustomSource.AddRange(new string[] {
+            "Sine",
+            "Cosine",
+            "Tangent",
+            "Logarithm"});
+            this.comboBoxFunctions.FormattingEnabled = true;
+            this.comboBoxFunctions.Items.AddRange(new object[] {
+            "Sine, Cosine, Tangent, Logarithm (Base 10)"});
+            this.comboBoxFunctions.Location = new System.Drawing.Point(46, 131);
+            this.comboBoxFunctions.Name = "comboBoxFunctions";
+            this.comboBoxFunctions.Size = new System.Drawing.Size(182, 24);
+            this.comboBoxFunctions.TabIndex = 0;
+            // 
+            // textInput
+            // 
+            this.textInput.Location = new System.Drawing.Point(46, 186);
+            this.textInput.Name = "textInput";
+            this.textInput.Size = new System.Drawing.Size(163, 22);
+            this.textInput.TabIndex = 1;
+            this.textInput.TextChanged += new System.EventHandler(this.textInput_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(69, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 42);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(266, 79);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(450, 239);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textInput);
+            this.Controls.Add(this.comboBoxFunctions);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxFunctions;
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Mid Term Lab/README.md
@@ -0,0 +1,4 @@
+# MidLab
+
+I have uploaded code files of Question 2 and 3
+The output file is also attached with outputs.docx file
